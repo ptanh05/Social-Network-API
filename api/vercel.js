cd54a5bd2a29851b75f74febcd10cd9534a43ef2
ptanh05 <ptanh05@gmail.com>
2026-04-15 17:21:07 +0700

    fix: remove conflicting api/vercel.js

--- api/vercel.js
+++
@@ -1,7 +0,0 @@
-import { createServer } from 'node:http';
-import app from './index.js';
-
-const server = createServer(app);
-server.listen(3000, () => {
-  console.log('Server ready on port 3000');
-});
